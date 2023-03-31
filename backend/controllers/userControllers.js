7627d2437e7f9e7ee71ac5624da4394f096ad52d
Harshita Gupta <sign.of.happiness@gmail.com>
2023-03-31 14:05:42 +0530

    created search user api and authorization middleware - Search user Api in userControllers.js and imported in userRoutes.js - Auth middleware - protct - in authMiddleware.js added protect along with allUsers in useRoutes

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -58,5 +58,22 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// api/user?search=harshita
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        // any one condition inside or has to be true
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+  // console.log(keyword);
+});
+
 // not a default export
-module.exports = { registerUser, authUser };
+module.exports = { registerUser, authUser, allUsers };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { registerUser, authUser } = require("../controllers/Usercontrollers");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/Usercontrollers");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 // route is used to chain multiple requests
-router.route("/").post(registerUser);
+// it has to go through the protect middleware before moving to allUsers
+router.route("/").post(registerUser).get(protect, allUsers);
 
 // directly adding post request (no chaining can be done here)
 router.post("/login", authUser);
